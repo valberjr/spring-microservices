dee03320fdba53089ec1b307159ad43fa3dd7615
valberjr <valber@duck.com>
2024-07-26 16:22:23 -0300

    test: add integration test for creating a product

--- product-service/src/test/java/com/example/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/example/product_service/ProductServiceApplicationTests.java
@@ -1,15 +1,47 @@
 package com.example.product_service;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void shouldCreateProduct() {
+
+        var requestBody = """
+                {
+                	"name": "iPhone 15",
+                	"description": "iPhone 15 is a smartphone from Apple",
+                	"price": 1000
+                }
+                """;
+
+        RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("iPhone 15"))
+                .body("description", Matchers.equalTo("iPhone 15 is a smartphone from Apple"))
+                .body("price", Matchers.equalTo(1000));
+    }
 
 }
