5b8c4421b304261705c6fdc8f076b3c7520f1b5e
valberjr <valber@duck.com>
2024-07-29 17:11:47 -0300

    feat: add endpoint for placing an order

---
+++ order-service/src/main/java/com/example/order_service/dto/OrderRequest.java
@@ -0,0 +1,10 @@
+package com.example.order_service.dto;
+
+import java.math.BigDecimal;
+
+public record OrderRequest(Long id,
+                           String orderNumber,
+                           String skuCode,
+                           BigDecimal price,
+                           Integer quantity) {
+}
