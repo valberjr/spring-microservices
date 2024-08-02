c109d68de10644092d0b72f952caa06b9e19d31d
valberjr <valber@duck.com>
2024-08-02 15:45:38 -0300

    fix: change Controller annotation to RestController

--- order-service/src/main/java/com/example/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -5,12 +5,12 @@ import com.example.order_service.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
 public class OrderController {
