308d27167e049c578549298eddede2d63aa0fd8b
valberjr <valber@duck.com>
2024-08-06 14:48:13 -0300

    feat: add circuit breaker to inventory client

--- order-service/src/main/java/com/example/order_service/config/RestClientConfig.java
+++ order-service/src/main/java/com/example/order_service/config/RestClientConfig.java
@@ -2,11 +2,17 @@ package com.example.order_service.config;
 
 import com.example.order_service.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.support.RestClientAdapter;
-import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
+
+import static org.springframework.web.client.support.RestClientAdapter.create;
+import static org.springframework.web.service.invoker.HttpServiceProxyFactory.builderFor;
 
 @Configuration
 public class RestClientConfig {
@@ -18,14 +24,17 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         var restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
-
-        var restClientAdapter = RestClientAdapter.create(restClient);
-
-        var httpServiceProxyFactory = HttpServiceProxyFactory
-                .builderFor(restClientAdapter)
-                .build();
-
+        var restClientAdapter = create(restClient);
+        var httpServiceProxyFactory = builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        var clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
     }
 }
