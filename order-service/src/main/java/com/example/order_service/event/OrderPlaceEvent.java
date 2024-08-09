851633f02fcd779dc68f802e4fee24aacd1a4e98
valberjr <valber@duck.com>
2024-08-09 17:04:13 -0300

    feat: create OrderPlaced event

---
+++ order-service/src/main/java/com/example/order_service/event/OrderPlaceEvent.java
@@ -0,0 +1,14 @@
+package com.example.order_service.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlaceEvent {
+
+    private String orderNumber;
+    private String email;
+}
