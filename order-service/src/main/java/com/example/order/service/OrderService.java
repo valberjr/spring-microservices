8d0001b27ed8be40e02e967462354e827a273dae
valberjr <valber@duck.com>
2024-08-12 15:02:00 -0300

    feat: integrate Kafka with schema registry

--- order-service/src/main/java/com/example/order/service/OrderService.java
+++ order-service/src/main/java/com/example/order/service/OrderService.java
@@ -2,7 +2,7 @@ package com.example.order.service;
 
 import com.example.order.client.InventoryClient;
 import com.example.order.dto.OrderRequest;
-import com.example.order.event.OrderPlaceEvent;
+import com.example.order.event.OrderPlacedEvent;
 import com.example.order.model.Order;
 import com.example.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +20,7 @@ public class OrderService {
     private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
-    private final KafkaTemplate<String, OrderPlaceEvent> kafkaTemplate;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -34,10 +34,16 @@ public class OrderService {
             orderRepository.save(order);
 
             // Send the message to kafka topic
-            OrderPlaceEvent orderPlaceEvent = new OrderPlaceEvent(order.getOrderNumber(), orderRequest.userDetail().email());
-            log.info("Start - sending OrderPlacedEvent {} to kafka topic order-placed", orderPlaceEvent);
-            kafkaTemplate.send("order-placed", orderPlaceEvent);
-            log.info("End - sending OrderPlacedEvent {} to kafka topic order-placed", orderPlaceEvent);
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(
+                    order.getOrderNumber(),
+                    orderRequest.userDetail().email(),
+                    orderRequest.userDetail().firstName(),
+                    orderRequest.userDetail().lastName()
+            );
+
+            log.info("Start - sending OrderPlacedEvent {} to kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - sending OrderPlacedEvent {} to kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
